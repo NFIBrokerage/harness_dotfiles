0f77e600ea395f87e06ea369832057e59bc32c53
Michael Davis <mcarsondavis@gmail.com>
2020-08-26 15:42:51 -0500

    use config api for fixture manifest

--- fixture/harness.exs
+++ fixture/harness.exs
@@ -1,8 +1,11 @@
-[
-  manifest_version: "1.0.0",
+import Config
+
+config :harness,
+  manifest_version: "2.0.0",
+  generators: [HarnessDotfiles],
   deps: [
     {:harness_dotfiles, path: "../"}
-  ],
-  generators: [HarnessDotfiles],
-  opts: []
-]
+  ]
+
+config :harness_dotfiles,
+  asdf_elixir_version: "1.10.3-otp-21"
