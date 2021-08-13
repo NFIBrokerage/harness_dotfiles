d9ef708c070fb935c99b4453fa39ce51c4015940
Tony van Riet <306897+tonyvanriet@users.noreply.github.com>
2021-08-13 15:00:09 -0500

    Formatter config includes priv dir (#7)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a
 Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## 1.0.1 - 2021-08-13
+
+### Added
+
+- Added `priv` dir to formatter inputs
+
 ## 1.0.0 - 2021-03-17
 
 ### Added
--- templates/.formatter.exs
+++ templates/.formatter.exs
@@ -1,7 +1,7 @@
 locals_without_parens = <%= Kernel.inspect(@formatter_locals_without_parens) %>
 
 [
-  inputs: ["mix.exs", "harness.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  inputs: ["mix.exs", "harness.exs", "{config,lib,test,priv}/**/*.{ex,exs}"],
   import_deps: <%= Kernel.inspect(@formatter_deps) %>,
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens],
